2ddfe5816923207a9d4f734f11fbc0c6ea60c642
shakori999 <shakori999@gmail.com>
2020-07-29 07:52:32 +0430

    inserting with bisect.insort

--- example2-1.py
+++ example2-1.py
@@ -223,17 +223,28 @@ import sys
 
 # example 2-18
 
-
-import bisect
-import sys
-
-
-def grade(score, breakpoint=[60, 70, 80, 90], grades="FDCBA"):
-    i = bisect.bisect(breakpoint, score)
-    return grades[i]
+# import bisect
+# import sys
 
 
-print([grade(score) for score in [33, 99, 77, 70, 12, 100]])
+# def grade(score, breakpoint=[60, 70, 80, 90], grades="FDCBA"):
+#     i = bisect.bisect(breakpoint, score)
+#     return grades[i]
+
+
+# print([grade(score) for score in [33, 99, 77, 70, 12, 100]])
 # ---------------------------------------------------------------
 
 # Inserting with bisect.insort
+# insort(seq, item)
+# import bisect
+# import random
+
+# size = 7
+# random.seed(1729)
+
+# my_list = []
+# for i in range(size):
+#     new_item = random.randrange(size * 2)
+#     bisect.insort(my_list, new_item)
+#     print("%2d ->" % new_item, my_list)
